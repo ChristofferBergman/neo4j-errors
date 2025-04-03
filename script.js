a5e675095c01f30de56a03199560cf120c88a780
ChristofferBergman <121811913+ChristofferBergman@users.noreply.github.com>
2025-04-03 12:46:56 +0200

    Restructured
    Changed names and added links to docs that gets updated depending on the code shown

--- script.js
+++ script.js
@@ -9,6 +9,18 @@ async function loadCSV(url) {
     return parsed.data;
 }
 
+// Update the link buttons
+async function updateLink(field, gqlCode) {
+    const docUrl = `https://neo4j.com/docs/status-codes/current/errors/gql-errors/${gqlCode}/`;
+    
+    if (gqlCode == '' || gqlCode.includes('|') || gqlCode.includes(',') || gqlCode.includes(' ') || gqlCode.length > 5) {
+        field.setAttribute('disabled', 'true');
+    } else {
+        field.removeAttribute('disabled');
+        field.href = docUrl;
+    }
+}
+
 // Main
 (async function () {
     const csvUrl = "https://raw.githubusercontent.com/ChristofferBergman/neo4j-errors/refs/heads/main/errors.csv";
@@ -24,27 +36,37 @@ async function loadCSV(url) {
     const searchInput = document.getElementById('searchMessage');
     const oldErrorField = document.getElementById('oldError');
     const gqlCodeField = document.getElementById('gqlCode');
-    const causeField = document.getElementById('cause');
+    const genericField = document.getElementById('gqlCodeGeneric');
+    const gqlLink = document.getElementById('gqlLink');
+    const genericLink = document.getElementById('genericLink');
     
     searchInput.addEventListener('input', () => {
         const query = searchInput.value.trim();
         if (!query) {
-            oldErrorField.value = gqlCodeField.value = causeField.value = '';
-            return;
+            oldErrorField.value = gqlCodeField.value = genericField.value = '';
+        } else {
+            const results = fuse.search(searchInput.value.trim());
+            if (results.length > 0) {
+                const bestMatch = results[0].item;
+                oldErrorField.value = bestMatch['Old Error'];
+                if (bestMatch['GQL'] == 'N/A') {
+                    gqlCodeField.value = 'This error is no longer used';
+                    genericField.value = '';
+                } else if (bestMatch['Cause'] == '') {
+                    // If cause is empty, set GQL as the detailed GQLSTATUS
+                    gqlCodeField.value = bestMatch['GQL'];
+                    genericField.value = '';
+                } else {
+                    // Otherwise, use the cause as the detailed GQLSTATUS
+                    gqlCodeField.value = bestMatch['Cause'];
+                    genericField.value = bestMatch['GQL'];
+                }
+            } else {
+                oldErrorField.value = gqlCodeField.value = causeField.value = '';
+            }
         }
         
-        const results = fuse.search(searchInput.value.trim());
-        if (results.length > 0) {
-            const bestMatch = results[0].item;
-            oldErrorField.value = bestMatch['Old Error'];
-            if (bestMatch['GQL'] == 'N/A') {
-                gqlCodeField.value = 'This error is no longer used';
-            } else {
-                gqlCodeField.value = bestMatch['GQL'];
-            }
-            causeField.value = bestMatch['Cause'];
-        } else {
-            oldErrorField.value = gqlCodeField.value = causeField.value = '';
-        }
+        updateLink(gqlLink, gqlCodeField.value);
+        updateLink(genericLink, genericField.value);
     });
 })();
