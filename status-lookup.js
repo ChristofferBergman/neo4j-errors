9185946362b377582ab54e98ece66baa3254ad61
ChristofferBergman <121811913+ChristofferBergman@users.noreply.github.com>
2025-04-15 13:42:13 +0200

    More information

--- status-lookup.js
+++ status-lookup.js
@@ -68,7 +68,29 @@ async function renderResultForStatus(status) {
         }
         exclusive = filtered;
         
-        if (missing.length === 0) {
+        // Find all GQLSTATUS codes used by this Neo4j Status (even if used by others)
+        let all = await fetchAllExclusiveCodes(session, status);
+        
+        // Again try to trim it
+        const filtered2 = [];
+        for (const code of all) {
+            const sublist = all.filter(c => c !== code);
+            const m = await fetchMissingErrors(session, status, sublist);
+            
+            if (m.length != 0) {
+                filtered2.push(code);
+            }
+        }
+        all = filtered2;
+        
+        // See what extra errors we would get by checking for all those
+        let extra = await fetchMissingErrors(session, status, exclusive);
+        
+        if (exclusive.length === 0) {
+            result.value = 'There are no GQLSTATUS codes that would only catch ' + status + ' errors\n';
+            addAlternative(status, all, extra);
+        }
+        else if (missing.length === 0) {
             result.value = 'You catch all errors of ' + status + ' by checking for these GQLSTATUS codes:\n';
             exclusive.forEach(code => result.value += ('\n * ' + code));
         } else {
@@ -77,6 +99,7 @@ async function renderResultForStatus(status) {
             result.value += '\n\nBut you would not catch these errors:\n';
             missing.forEach(code => result.value += ('\n   \"' + code + '\"'));
             result.value += '\n\nThe reason is that the GQLSTATUS of those are shared with other Neo4j Status codes\n';
+            addAlternative(status, all, extra);
         }
     } catch (error) {
         console.error("Neo4j query error:", error);
@@ -85,6 +108,13 @@ async function renderResultForStatus(status) {
         await session.close();
         await driver.close();
     }
+}
+
+function addAlternative(status, all, extra) {
+    result.value += '\nTo catch all ' + status + ' errors you would need to check for all these\n';
+    all.forEach(code => result.value += ('\n * ' + code));
+    result.value += '\n\nBut that would also give you these errors that is not of this status:\n';
+    extra.forEach(code => result.value += ('\n   \"' + code + '\"'));
 }
 
 async function fetchAllExclusiveCodes(session, status) {
@@ -104,6 +134,17 @@ async function fetchAllExclusiveCodes(session, status) {
     return result.records.map(record => record.get('code'));
 }
 
+async function fetchAllCodes(session, status) {
+    const result = await session.executeRead(tx => tx.run(
+        `MATCH (e:Error)-[:NEO4JSTATUS]->(n:Neo4jStatus {code: $status}),
+        (e)-[:GQLSTATUS|CAUSE]->(g:GqlStatus)
+        WHERE g.code <> "N/A"
+        RETURN DISTINCT g.code AS code
+        `, {status}));
+    
+    return result.records.map(record => record.get('code'));
+}
+
 async function fetchMissingErrors(session, status, codes) {
     const result = await session.executeRead(tx => tx.run(
         `MATCH (e:Error)-[:NEO4JSTATUS]->(n:Neo4jStatus {code: $status})
@@ -111,6 +152,18 @@ async function fetchMissingErrors(session, status, codes) {
         WHERE g.code IN $codes
         WITH e, COUNT(g) AS c
         WHERE c = 0
+        RETURN e.olderror AS error
+        `, {status, codes}));
+    
+    return result.records.map(record => record.get('error'));
+}
+
+async function fetchExtraErrors(session, status, codes) {
+    const result = await session.executeRead(tx => tx.run(
+        `MATCH (e:Error)-[:NEO4JSTATUS]->(n:Neo4jStatus)
+        WHERE n.code <> status
+        MATCH (e)-[:GQLSTATUS|CAUSE]->(g:GqlStatus)
+        WHERE g.code IN $codes
         RETURN e.olderror AS error
         `, {status, codes}));
     
